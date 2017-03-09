d8f270e556c47918de00aea9b7ba6f9d5e05e7ca
eduberrueta <eduestella@hotmail.com>
2017-03-09 09:32:18 +0100

    Subo el nuevo xirsys con usuario contraseña, room...

--- 5-simplewebrtc-signaling/demo/xirsys_connect.js
+++ 5-simplewebrtc-signaling/demo/xirsys_connect.js
@@ -1,27 +1,30 @@
 // 'ident' and 'secret' should ideally be passed server-side for security purposes.
 // If secureTokenRetrieval is true then you should remove these two values.
+//Username eduberrueta
+//Correo eduestella@hotmail.com
+//contraseña berrueta
 
 // Insecure method
-var xirsysConnect = {
+/**var xirsysConnect = {
 	secureTokenRetrieval : false,
 	data : {
-		channel : 'channel1',
-		ident : 'markogalarza',
-		secret : '336a697a-eef7-11e6-a4ef-1394ae5277b9'
+		channel : 'www.xirsys.com',
+		ident : 'LeeSylvester',
+		secret : '84ddc270-c62a-11e6-9f07-2507c0291f3c'
 	}
-};
-/*
+};**/
+
 var xirsysConnect = {
 	secureTokenRetrieval : false,
 	data : {
-		domain : 'dsm2017',
-		application : 'default',
+		domain : 'berruetaBeloso',
+		application : 'practicaDSM',
 		room : 'default',
-		ident : 'markogalarza',
-		secret : '8ef83a62-eb17-11e6-bee9-09d9990f7fd5',
+		ident : 'eduberrueta',
+		secret : '5a6a4490-04a1-11e7-8586-1443c74ef2b0',
 		secure : 0
 	}
-};*/
+};
 
 // Secure method
 /*var xirsysConnect = {
