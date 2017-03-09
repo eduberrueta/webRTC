cbd2a99da475fa7c0c595d1b5e13cf788c033c2f
eduberrueta <eduestella@hotmail.com>
2017-03-09 10:06:24 +0100

    mas cambios para intentar arreglarlo

--- 5-simplewebrtc-signaling/demo/xirsys_connect.js
+++ 5-simplewebrtc-signaling/demo/xirsys_connect.js
@@ -1,30 +1,27 @@
 // 'ident' and 'secret' should ideally be passed server-side for security purposes.
 // If secureTokenRetrieval is true then you should remove these two values.
-//Username eduberrueta
-//Correo eduestella@hotmail.com
-//contraseña berrueta
 
 // Insecure method
-/**var xirsysConnect = {
+var xirsysConnect = {
 	secureTokenRetrieval : false,
 	data : {
-		channel : 'www.xirsys.com',
-		ident : 'LeeSylvester',
-		secret : '84ddc270-c62a-11e6-9f07-2507c0291f3c'
+		channel : 'berruetabeloso',
+		ident : 'eduberru',
+		secret : 'ead5c1b2-04a6-11e7-85ea-c94e017895ee'
 	}
-};**/
+};
 
-var xirsysConnect = {
+/*var xirsysConnect = {
 	secureTokenRetrieval : false,
 	data : {
 		domain : 'berruetabeloso',
 		application : 'practicadsm',
 		room : 'practicawebrtc',
-		ident : 'eduberrueta',
-		secret : '5a6a4490-04a1-11e7-8586-1443c74ef2b0',
+		ident : 'eduberru',
+		secret : 'ead5c1b2-04a6-11e7-85ea-c94e017895ee',
 		secure : 0
 	}
-};
+};*/
 
 // Secure method
 /*var xirsysConnect = {
